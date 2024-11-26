e8357fe028b09ae97268322351b411ecac935996
Gustavo Rincha <gustavo.rincha@gmail.com>
2024-11-26 17:09:42 -0300

    Exemplos de tamplate temp

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': ' Gustavo Rincha'
+    })
+
+def contato(request):
+    return render(request, 'temp.html')
+
+def sobre(request):
+    return HttpResponse('sobre')
